6b0a361fb27bea72868ac1389fb567ddd4d815f8
Thalles <thalleslimabr@gmail.com>
2024-12-01 20:31:43 -0300

    adcionado plano de fundo e o botão resetar

--- app/src/main/java/com/example/contadores/MainActivity.kt
+++ app/src/main/java/com/example/contadores/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,14 +15,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.contadores.ui.theme.ContadoresTheme
@@ -45,17 +46,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting( modifier: Modifier = Modifier) {
-    var count: Int by remember { mutableStateOf<Int>(0) };
+    var count: Int by remember { mutableIntStateOf(0) };
+
+    Image(
+        painter = painterResource(id = R.drawable.bmw),
+        contentDescription = "Plano de fundo",
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop,
+    )
 
     Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp).background(Color.White),
+        modifier = Modifier.fillMaxSize().padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
         Text(
             text = count.toString(),
             modifier = Modifier.padding(0.dp).align(Alignment.CenterHorizontally),
-            color = Color.Blue,
+            color = Color.Red,
             fontFamily = FontFamily.SansSerif,
             fontSize = 60.sp,
         )
@@ -63,9 +71,14 @@ fun Greeting( modifier: Modifier = Modifier) {
             onClick = { count++ },
             modifier = Modifier.padding(25.dp).fillMaxWidth()
         ) {
-            Text(text = "Contar", fontSize = 30.sp)
+            Text(text = "Contar", fontSize = 30.sp);
+        }
+        Button(
+            onClick = { count = 0 },
+            modifier = Modifier.padding(25.dp).fillMaxWidth()
+        ) {
+            Text(text = "Resetar", fontSize = 30.sp);
         }
 
     }
 }
-
